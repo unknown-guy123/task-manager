300519f191c27a2534651ffeb56e8f76f68de7d5
unknown-guy123 <gitearyan01@gmail.com>
2024-12-19 18:41:16 +0530

    added_file_handling

--- TaskManager.py
+++ TaskManager.py
@@ -1,15 +1,19 @@
-
 class task():
     def __init__(self):
         self.id = 0
         self.task = ""
         self.status = "Pending..."
 
+
 UserTask = task()
 
 TaskNumber = input("\nEnter the Task Number: ")
 repeat = int(TaskNumber)
-
+TaskDataFile = open("TaskDataFile.txt", "w")
 for task in range(repeat):
+    TaskDataFile = open("TaskDataFile.txt", "a+")
     UserTask.task = input(f"Task {UserTask.id + 1}: ")
     UserTask.id += 1
+    TaskDataFile.write("\n" + str(UserTask.id) + "\t")
+    TaskDataFile.write(UserTask.task + "\t")
+    TaskDataFile.write(UserTask.status + "\t")
